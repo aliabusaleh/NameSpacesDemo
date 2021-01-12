2e407f73f87e19950b88a1f3386391784fe9152e
Ali AbuSaleh <ali.abusaleh.18@um.edu.mt>
2021-01-12 11:44:45 +0200

    first commit

---
+++ NamespaceDemo2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myNameSpaceDemo1
+{
+    enum Tempreture
+    {
+        Cold = 0,
+        Warm = 1,
+        Hot = 2
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using myNameSpaceDemo1;
+
+namespace NameSpaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Level level = Level.Low;
+            Tempreture tempreture = Tempreture.Cold;
+            Console.WriteLine(level);
+            Console.WriteLine(tempreture);
+            Console.ReadKey();
+        }
+    }
+}
